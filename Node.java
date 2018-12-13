4930ca7dae2b6fca5d8f0393c8c2b11a897dfda1
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-12-13 11:34:14 -0500

    hi

--- Node.java
+++ Node.java
@@ -1,20 +1,44 @@
-class Node{
- private int data;
- private Node next,prev;
-}
 
 
-class MyLinkedList{
- private int size;
- private Node start,end;
+public class MyLinkedList{
+  private class Node{
+    private int data;
+    private Node next,prev;
+      public Node next(){
+        return next;
+      }
+      public Node prev(){
+        return prev;
+      }
+      public void setNext(Node other){
+        next = other;
+      }
+      public void setPrev(Node other){
+        prev = ther;
+      }
+      public Integer getData(){
+        return data;
+      }
+      public Integer setData(Integer i){
+        data = i;
+        return data;
+      }
+      public String toString(){
+        String y = "";
+        return y + data;
+      }
+   }
+   private int size;
+   private Node start,end;
 
- public int size() {
-   return size;
+  // MyLinkedList() - make an empty list.
+   public int size() {
+     return size;
+   }
+   public boolean add(int value){
+     return true;
+   }
+   public String toString(){
+     return "[ " + start + " | " + " ]";
+   }
  }
- public boolean add(int value){
-   return true;
- }
- public String toString(){
-   return "[ " + start + " | " + " ]";
- }
-}
